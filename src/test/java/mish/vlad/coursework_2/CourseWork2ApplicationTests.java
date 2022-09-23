eb89f336b01fa03ebacd4d8aa8b148ed4cbd3085
VMishutkin <vladmish@list.ru>
2022-09-23 14:47:20 +0700

    Initial commit

---
+++ src/test/java/mish/vlad/coursework_2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mish.vlad.coursework_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
